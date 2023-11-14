b2c6384a0ec1aabf4e72a4e7fb7b2d7c878c6329
azrwinata <116279788+azrwinata@users.noreply.github.com>
2023-11-14 11:29:21 +0700

    Add files via upload

---
+++ Modul3/connect.php
@@ -0,0 +1,14 @@
+<!-- File ini berisi koneksi dengan database yang telah di import ke phpMyAdmin kalian -->
+
+
+<?php
+// Buatlah variable untuk connect ke database yang telah di import ke phpMyAdmin
+$conn = mysqli_connect("localhost:3308", "root", "", "wad_modul3_mas");
+
+
+// 
+  
+// Buatlah perkondisian jika tidak bisa terkoneksi ke database maka akan mengeluarkan errornya
+
+// 
+?>
